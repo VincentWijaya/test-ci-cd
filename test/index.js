b7452541f8314099f88c682bd44ece728d2ee8d8
Vincent Wijaya <vincent@myboost.id>
2019-01-17 10:17:38 +0700

    adding unit test

---
+++ test/index.js
@@ -0,0 +1,25 @@
+const app = require('../app');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('/', () => {
+  it('Should return hello world', (done) => {
+    chai.request(app)
+      .get('/')
+      .then((res) => {
+        const { body } = res;
+
+        body.should.be.a('object');
+        body.message.should.equal('Hello world');
+        body.success.should.equal(true);
+
+        done();
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  });
+});
